7413ca2fea88afd6b3ecbdfe7b7e8b260f0b511c
colinstark <starkinbox@gmail.com>
2025-01-30 22:54:36 +0100

    added multiple strategies

---
+++ configs.js
@@ -0,0 +1,42 @@
+// const colors = ["#222266", "#F9D7DE", "#BEEDFF", "#FFD9C4"] // alt set for podcastle
+const colors = [
+	"#0081A7",
+	"#00AFB9",
+	"#ADACDC",
+	"#FED9B7",
+	"#E7C8DD",
+	"#BEEDFF",
+	"#22D9C4",
+	"#4381C1",
+	"#17BEBB",
+	"#D62246"
+]
+const gradientColor = "#FFFFFF"
+const strategies = [
+	"centered",
+	"overscale",
+	"offset-left",
+	"offset-right",
+	"offset-top",
+	"offset-bottom",
+	"mirror",
+	"mirror-offset"
+];
+const colorStrategies = [
+	"solid",
+	"fade",
+	"fade",
+	"fade",
+	"fade",
+	"fade-reverse"
+]
+const shapes = [
+	"blob1",
+	"blob2",
+	"circle",
+	"loop",
+	"squares",
+	"triangles",
+	"trismooth",
+	"wave"
+]
--- main.js
+++ main.js
@@ -1,63 +1,159 @@
-const colors = ["222266", "#F9D7DE", "#BEEDFF", "#FFD9C4"];
-let name = "NAME";
-let tags = "ambient, test";
-let nameSum = 0;
-let tagSum = 0;
-
-let params = {
-	color: "000000",
-	shape: "circle",
-	strategy: "overscale", // overscale, mirror, mirror-offset
-	flip: "false"
+let inputs = {
+	name: "NAME",
+	tags: "tag, tag",
+	_nameSum: 0,
+	_tagSum: 0
 }
 
-function update(newName, newTags){
-	updateName(newName)
-	updateTags(newTags)
+let params = {
+	color: colors[0],
+	shape: shapes[0],
+	strategy: strategies[0],
+	colorStrategy: colorStrategies[0],
+	flip: false
+}
+
+function update(newName, newTags) {
+	inputs = {
+		...inputs,
+		name: newName,
+		tags: newTags,
+		_nameSum: calculateSum(newName),
+		_tagSum: calculateSum(newTags)
+	}
+
+	params = {
+		...params,
+		color: getSpecificValueOf(colors, inputs._nameSum),
+		shape: getSpecificValueOf(shapes, inputs._tagSum),
+		strategy: getSpecificValueOf(strategies, inputs._nameSum + inputs._tagSum),
+		colorStrategy: getSpecificValueOf(colorStrategies, inputs._nameSum + inputs._tagSum),
+		flip: (inputs._nameSum + inputs._tagSum) % 2 == 0
+	}
+
+	ctx.width = canvas.width
+	ctx.height = canvas.height
+
+	console.log(params, inputs)
 	paint(ctx)
 }
 
-function updateName(str){
-	name = str
-	nameSum = calculateSum(str)
-	params.color = getColor(name, colors)
-}
-
-function updateTags(str){
-	tags = str
-	tagSum = calculateSum(str)
-}
-
-function getColor(str, colors){
-	return colors[nameSum % colors.length]
-}
-
-function calculateSum(name){
+function calculateSum(name) {
 	return name.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0)
 }
 
-function paint(ctx){
-	ctx.fillStyle = params.color;
-	ctx.fillRect(0, 0, 512, 512);
-
-	// ctx.scale(2,2)
-	// ctx.translate(-180, -80);
-	// ctx.fill(p2);
+function getSpecificValueOf(arr, hash) {
+	console.log("getting", arr[hash % arr.length], arr.length)
+	return arr[hash % arr.length]
 }
 
-function drawInlineSVG(svgElement, ctx, callback) {
-  var svgURL = new XMLSerializer().serializeToString(svgElement);
-  var img = new Image();
-  img.onload = function() {
-    ctx.drawImage(this, 0, 0);
-    callback();
-  }
-  img.src = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(svgURL);
+function paint(ctx) {
+	ctx.globalAlpha = 1;
+	ctx.fillStyle = params.color
+	ctx.fillRect(0, 0, ctx.width, ctx.height)
+
+	ctx.fillStyle = makeGradient(params.color, 0, ctx.height, ctx)
+	ctx.fillRect(0, 0, ctx.width, ctx.height)
+
+	ctx.globalAlpha = 0.1;
+	ctx.fillStyle = params.color
+	ctx.fillRect(0, 0, ctx.width, ctx.height)
+	ctx.globalAlpha = 1;
+
+	var svg = new Blob([shape(params.shape, params.colorStrategy, gradientColor)], {
+		type: "image/svg+xml;charset=utf-8"
+	});
+	var url = URL.createObjectURL(svg);
+
+	let img = new Image();
+	img.addEventListener('load', e => {
+		let target = getShapeImageParameters(e.target)
+		console.log("FIRST", target)
+		ctx.scale(1, 1)
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+		ctx.drawImage(e.target, target.x, target.y, target.width, target.height);
+	});
+	img.src = url;
+
+	if (["mirror", "mirror-offset"].includes(params.strategy)) {
+		console.log("its a dual")
+
+		let img2 = new Image();
+		img2.addEventListener('load', e => {
+			let target = getShapeImageParameters(e.target, true)
+			console.log("SECOND", target)
+			ctx.scale(-1, -1)
+			ctx.drawImage(e.target, target.x, target.y, target.width, target.height);
+			ctx.scale(1, 1)
+			ctx.setTransform(1, 0, 0, 1, 0, 0);
+		});
+		img2.src = url;
+
+
+	}
+
 }
 
-function makeGradient(color, bg_color, to_x, to_y, ctx){
+function getShapeImageParameters(target, second = false) {
+	let upscaleFactor = getUpscaleFactor(target)
+	let scale = 0.7
+	let offsetX = 0.5
+	let offsetY = 0.5
+
+	switch (params.strategy) {
+		case "overscale":
+			scale = 0.8
+			break;
+
+		case "offset-left":
+			scale = 1.3
+			offsetX = 0.1
+			break;
+		case "offset-right":
+			scale = 1.3
+			offsetX = 0.9
+			break;
+		case "offset-top":
+			scale = 1.3
+			offsetY = 0.1
+			break;
+		case "offset-bottom":
+			scale = 1.3
+			offsetY = 0.9
+			break;
+
+		case "mirror":
+			scale = 1
+			offsetX = second ? -0.4 : 0.6
+			offsetY = second ? -0.4 : 0.6
+			break;
+
+		case "mirror-offset":
+			scale = 1
+			offsetX = second ? -0.2 : 0.8
+			offsetY = second ? -0.2 : 0.8
+			break;
+
+	}
+
+	return {
+		x: (ctx.width * offsetX) - (target.width * upscaleFactor * scale / 2),
+		y: (ctx.height * offsetY) - (target.height * upscaleFactor * scale / 2),
+		width: target.width * upscaleFactor * scale,
+		height: target.height * upscaleFactor * scale
+	}
+
+}
+
+function getUpscaleFactor(target) {
+	let scaleX = (ctx.width / target.width)
+	let scaleY = (ctx.height / target.height)
+	return scaleX > scaleY ? scaleY : scaleX
+}
+
+function makeGradient(color, to_x, to_y, ctx) {
 	let gradient = ctx.createLinearGradient(0, 0, to_x, to_y);
-	gradient.addColorStop(0, selectedColor);
+	gradient.addColorStop(0, color);
 	gradient.addColorStop(1, "#FFFFFF");
 	return gradient
 }
